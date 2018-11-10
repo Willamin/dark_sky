7c1f795a6852778ce8d1257c2306b786a6e1c93b
Will Lewis <will@wflewis.com>
2018-11-09 19:31:13 -0500

    :sparkles: initial formation

---
+++ spec/spec_helper.cr
@@ -0,0 +1,4 @@
+require "../src/dark-sky"
+require "microtest"
+include Microtest::DSL
+Microtest.run!
---
+++ src/dark-sky.cr
@@ -0,0 +1,3 @@
+module DarkSky
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+end
